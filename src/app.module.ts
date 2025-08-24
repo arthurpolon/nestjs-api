d35db5eaa32420dcbca1decea4fb28abde7636f7
Arthur Polon <arthurpolon@hotmail.com>
2025-08-23 21:17:46 -0400

    add docker and env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Env, validate } from 'src/config/env.validator';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService<Env>) => {
+        return {
+          type: 'postgres',
+          host: config.get('DB_HOST', { infer: true }),
+          port: config.get('DB_PORT', { infer: true }),
+          username: config.get('DB_USERNAME', { infer: true }),
+          password: config.get('DB_PASSWORD', { infer: true }),
+          database: config.get('DB_NAME', { infer: true }),
+          entities: [],
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Env } from 'src/config/env.validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService<Env>);
+  const port = configService.get('PORT', { infer: true });
+
+  await app.listen(port ?? 3001);
 }
-bootstrap();
+void bootstrap();
